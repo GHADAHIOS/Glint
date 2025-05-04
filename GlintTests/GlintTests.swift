9aba6322250ef72848a80ad0c1046ee8547cd72e
GHADAHIOS <ghada515alenzi515@gmail.com>
2025-05-04 21:26:44 +0300

    Initial Commit

---
+++ GlintTests/GlintTests.swift
@@ -0,0 +1,17 @@
+//
+//  GlintTests.swift
+//  GlintTests
+//
+//  Created by GHADAH ALENEZI on 06/11/1446 AH.
+//
+
+import Testing
+@testable import Glint
+
+struct GlintTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
